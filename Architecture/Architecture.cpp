849467c0729158d97fb06b91287fdb0934f3f227
Maxim Suntsev <masuntsev@edu.hse.ru>
2017-09-11 21:53:32 +0500

    Опробована работа с локалями.

---
+++ Architecture/Architecture.cpp
@@ -0,0 +1,27 @@
+#include "stdafx.h"
+
+#include <iosfwd>
+#include <locale>
+
+int main()
+{
+    using std::wcin;
+    using std::wcout;
+    using std::locale;
+
+    // Создаем россияновскую локаль.
+    locale russian = locale("ru-RU.CP1251");
+
+    // Устанавливаем ее как глобальную.
+    locale::global(russian);
+
+    int number;
+
+    // Вводим обычный номер.
+    wcin >> number;
+
+    // Выводим текст на российском и номер.
+    wcout << L"Номер равняется " << number << L".";
+
+    return 0;
+}
